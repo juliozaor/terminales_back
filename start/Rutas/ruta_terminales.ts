5b82965f19e71aebefbcd2ff7c51fbd5306cc33b
kevinsoto772 <kosotosimanca@gmail.com>
2024-10-11 17:56:36 -0500

    Cambios api rutas

--- start/Rutas/ruta_terminales.ts
+++ start/Rutas/ruta_terminales.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/quotes */
 /* eslint-disable @typescript-eslint/naming-convention */
 import Route from '@ioc:Adonis/Core/Route'
-const accion_path = '../../../app/Presentacion/terminales/ControladorTerminales'
+const accion_path = '../../../app/Presentacion/Terminales/ControladorTerminales'
 
 
 Route.group(() => {
